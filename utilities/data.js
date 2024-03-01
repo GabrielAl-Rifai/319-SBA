58eb3d220a55a6aa0358eee8d026a42816b60d7a
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-03-01 14:43:39 -0600

    update data.js

---
+++ utilities/data.js
@@ -0,0 +1,9 @@
+const climbs = [
+  { name: "Bllod Sacrifice", difficulty: "5.10d", accessible: true },
+  { name: "El Machete", difficulty: "5.10d", accessible: true },
+  { name: "Good Book", difficulty: "5.10d", accessible: true },
+  { name: "Wasp Arete", difficulty: "5.10d", accessible: true },
+  { name: "Use The Force Luke", difficulty: "5.10d", accessible: true },
+];
+
+module.exports = climbs;
